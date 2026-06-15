f3222e2ba386cdea94c0a3f9bf353b2b6a4c9090
Arthur801 <59018955+Arthur801@users.noreply.github.com>
2026-06-15 16:40:39 +0800

    Finish the task 2 and 3 in assign2 by python

--- week 2/assign2.py
+++ week 2/assign2.py
@@ -32,7 +32,131 @@ func1("特南克斯")
 
 
 # task 2
+schedule = None
+def func2(ss, start, end, criteria):
+    global schedule
+    if schedule == None:
+        schedule = [[False] * 24 for s in range(len(ss))]# record the schedule of ss
+    
+    closest = [None, float("inf"), -1]
 
-
-
+    if criteria[0] == "c":
+        if criteria[1:3] == ">=":
+            c = int(criteria[3:])
+            for i in range(len(ss)):
+                closest_dist = abs(ss[i]["c"]-c)
+                if ss[i]["c"] >= c and closest_dist < closest[1] and all(t == False for t in schedule[i][start:end+1]):
+                    closest = [ss[i], closest_dist, i]
+            if closest[0] != None:
+                schedule[closest[2]][start:end] = [True]*(end-start)
+                print(ss[closest[2]]["name"])
+            else:
+                print("Sorry")
+        elif criteria[1:3] == "<=":
+            c = int(criteria[3:])
+            for i in range(len(ss)):
+                closest_dist = abs(ss[i]["c"]-c)
+                if ss[i]["c"] <= c and closest_dist < closest[1] and all(t == False for t in schedule[i][start:end+1]):
+                    closest = [ss[i], closest_dist, i]
+            if closest[0] != None:
+                schedule[closest[2]][start:end] = [True]*(end-start)
+                print(ss[closest[2]]["name"])
+            else:
+                print("Sorry")
+        elif criteria[1] == "=":
+            c = int(criteria[2:])
+            for i in range(len(ss)):
+                closest_dist = abs(ss[i]["c"]-c)
+                if ss[i]["c"] == c and closest_dist < closest[1] and all(t == False for t in schedule[i][start:end+1]):
+                    closest = [ss[i], closest_dist, i]
+            if closest[0] != None:
+                schedule[closest[2]][start:end] = [True]*(end-start)
+                print(ss[closest[2]]["name"])
+            else:
+                print("Sorry")
+        else:
+            print("err")
+    elif criteria[0] == "r":
+        if criteria[1:3] == ">=":
+            r = float(criteria[3:])
+            for i in range(len(ss)):
+                closest_dist = abs(ss[i]["r"]-r)
+                if ss[i]["r"] >= r and closest_dist < closest[1] and all(t == False for t in schedule[i][start:end+1]):
+                    closest = [ss[i], closest_dist, i]
+            if closest[0] != None:
+                schedule[closest[2]][start:end] = [True]*(end-start)
+                print(ss[closest[2]]["name"])
+            else:
+                print("Sorry")
+        elif criteria[1:3] == "<=":
+            r = float(criteria[3:])
+            for i in range(len(ss)):
+                closest_dist = abs(ss[i]["r"]-r)
+                if ss[i]["r"] <= r and closest_dist < closest[1] and all(t == False for t in schedule[i][start:end+1]):
+                    closest = [ss[i], closest_dist, i]
+            if closest[0] != None:
+                schedule[closest[2]][start:end] = [True]*(end-start)
+                print(ss[closest[2]]["name"])
+            else:
+                print("Sorry")
+        elif criteria[1] == "=":
+            r = float(criteria[2:])
+            for i in range(len(ss)):
+                closest_dist = abs(ss[i]["r"]-r)
+                if ss[i]["r"] == r and closest_dist < closest[1] and all(t == False for t in schedule[i][start:end+1]):
+                    closest = [ss[i], closest_dist, i]
+            if closest[0] != None:
+                schedule[closest[2]][start:end] = [True]*(end-start)
+                print(ss[closest[2]]["name"])
+            else:
+                print("Sorry")
+        else:
+            print("err")
+    elif criteria[0:4] == "name":
+        for i in range(len(ss)):
+            if ss[i]["name"] == criteria[5:] and all(t == False for t in schedule[i][start:end+1]):
+                schedule[i][start:end] = [True]*(end-start)
+                print(ss[i]["name"])
+        else:
+            print("Sorry")
+    else:
+        print("err")
+    
+        
+services = [
+    {"name":"S1", "r":4.5, "c":1000},
+    {"name":"S2", "r":3, "c":1200},
+    {"name":"S3", "r":3.8, "c":800}
+]
 print("==========Task 2==========")
+func2(services, 15, 17, "c>=800")
+func2(services, 11, 13, "r<=4")
+func2(services, 10, 12, "name=S3")
+func2(services, 15, 18, "r>=4.5")
+func2(services, 16, 18, "r>=4")
+func2(services, 13, 17, "name=S1")
+func2(services, 8, 9, "c<=1500")
+
+# task 3
+def func3(index):
+    # The rule of number sequence : (-2, -3, +1, +2)*n
+    num = 25
+    match index%4:
+        case 0:
+            print(num - 2*(index//4))
+        case 1:
+            print(num - 2*(index//4) - 2)
+        case 2:
+            print(num - 2*(index//4) - 5)
+        case 3:
+            print(num - 2*(index//4) - 4)
+        case _:
+            print("error")
+            return -1
+
+
+print("==========Task 3==========")
+func3(1);
+func3(5);
+func3(10);
+func3(30);
