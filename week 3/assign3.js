8dae9722b9a782c4eddb1231c7838d26ac7b4d7e
Arthur801 <59018955+Arthur801@users.noreply.github.com>
2026-06-26 15:21:51 +0800

    Finish task 3 in week 3

--- week 3/assign3.js
+++ week 3/assign3.js
@@ -1,4 +1,5 @@
 // 取得資料
+const SITE = "https://cwpeng.github.io/test"
 let attrs = fetch("https://cwpeng.github.io/test/assignment-3-1")
     .then((response) => response.json())
     .then(data => data.rows)
@@ -15,24 +16,77 @@ let attrsPicture = fetch("https://cwpeng.github.io/test/assignment-3-2")
 // 合併資料
 Promise.all([attrs, attrsPicture])
     .then(([attractions, pictures]) => {
-        // console.log(attractions);
         for(let attr of attractions) {
             let picture = pictures.find(picture => picture.serial === attr.serial);
             const firstImage = picture.pics.match("/.*?\.jpg")[0];
             attr.pic = firstImage;
+            console.log(attr);
+        }
+        // 顯示bar
+        const bars = document.querySelector(".bars");
+        for(let i = 0; i < 3; i++) {
+            const bar = createBar(attractions[i]);
+            bars.appendChild(bar);
+        }
+        // 顯示block
+        const contentBlocks = document.querySelector(".content-blocks");
+        for(let i = 0; i < 10; i++) {
+            const block = createBlock(attractions[3+i]);
+            contentBlocks.appendChild(block);
         }
     });
-    
-// 顯示bar
+
 
 
 // 顯示blocks
 
 
 
-function createBar(attraction) {}
+function createBar(attraction) {
+    const bar = document.createElement("div");
+    bar.className = "bar";
 
-function createBlock(attraction) {}
+    // img
+    const barImg = document.createElement("img");
+    barImg.src = `${SITE}${attraction.pic}`;
+    barImg.alt = attraction.sname;
+
+    // text
+    const barText = document.createElement("p");
+    barText.className = "bar-text";
+    barText.textContent = attraction.sname;
+
+    bar.appendChild(barImg);
+    bar.appendChild(barText);
+
+    return bar;
+}
+
+function createBlock(attraction) {
+    const block = document.createElement("div");
+    block.className = "block";
+
+    // img
+    const blockImg = document.createElement("img");
+    blockImg.src = `${SITE}${attraction.pic}`;
+    blockImg.alt = attraction.sname;
+
+    // star
+    const star = document.createElement("span");
+    star.className = "star";
+    star.textContent = "⭐️"
+
+    // block text
+    const blockText = document.createElement("div");
+    blockText.className = "block-text";
+    blockText.textContent = attraction.sname;
+
+    block.appendChild(blockImg);
+    block.appendChild(star);
+    block.appendChild(blockText);
+    
+    return block;
+}
 
 function renderBars(attraction) {}
 
