36c2e8632943a07fa7c4eab4fbe9924688f57df9
Arthur801 <59018955+Arthur801@users.noreply.github.com>
2026-07-03 10:17:39 +0800

    Finish task 2 in week4

--- week4/main.py
+++ week4/main.py
@@ -1,7 +1,57 @@
-from fastapi import FastAPI
-from jinja2 import Template
+from typing import Annotated
+
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app=FastAPI()
 
-@app.get("/")
-def index():
-    return [100, 200, 300]
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+USER_DATABASE = {
+    "abc@abc.com" : "abc"
+}
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request, 
+        name="index.html"
+    )
+
+@app.post("/signin")
+async def signin(email: Annotated[str, Form()], password: Annotated[str, Form()]):
+    if email == "" or password == "":
+        return RedirectResponse(
+            url="/ohoh?msg=請輸入信箱和密碼",
+            status_code=307
+        )
+    
+    if email in USER_DATABASE:
+        if password == USER_DATABASE[email]:
+            return RedirectResponse(
+                url="/member", 
+                status_code=303
+            )
+    return RedirectResponse(
+        url="/ohoh?msg=信箱或密碼輸入錯誤",
+        status_code=303
+    )
+    
+@app.get("/member")
+async def member(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="member.html"
+    )
+
+@app.get("/ohoh")
+async def ohoh(request: Request, msg: str = ""):
+    return templates.TemplateResponse(
+        request=request,
+        name="ohoh.html",
+        context={"msg": msg}
+    )
