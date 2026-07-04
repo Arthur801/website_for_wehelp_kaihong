de238eeaa74aab488eb1f9a04dac8aea39d87eb8
Arthur801 <59018955+Arthur801@users.noreply.github.com>
2026-07-04 17:26:56 +0800

    Add comment in weed4 main.py

--- week4/main.py
+++ week4/main.py
@@ -29,17 +29,21 @@ for hotel in hotelsListEN:
 for hotel in hotelsListCH:
     hotelsDict[hotel["_id"]].update({"hotelNameCH":hotel["旅宿名稱"], "phone":hotel["電話或手機號碼"]})
 
+# 建立fastapi server
 app=FastAPI()
 
+# 連結靜態資料與建立確認網站是否登入的middleware
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(SessionMiddleware, secret_key="gjapsfu1")
-
+# 取得jinja2模板資料夾（html)
 templates = Jinja2Templates(directory="templates")
 
+# 模擬使用者帳號資料庫
 USER_DATABASE = {
     "abc@abc.com" : "abc"
 }
 
+# 連進首頁
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(
@@ -47,9 +51,11 @@ async def index(request: Request):
         name="index.html"
     )
 
+# 首頁登入
 @app.post("/login")
 async def login(request: Request, email: Annotated[str, Form()] = "", password: Annotated[str, Form()] = ""):
     if email == "" or password == "":
+        # 為填入資料導向錯誤頁面，並傳入錯誤訊息
         return RedirectResponse(
             url="/ohoh?msg=請輸入信箱和密碼",
             status_code=303
@@ -58,27 +64,33 @@ async def login(request: Request, email: Annotated[str, Form()] = "", password: 
     if email in USER_DATABASE:
         if password == USER_DATABASE[email]:
             request.session["LOGGED-IN"] = True
+            # 判斷登入成功後導向使用者頁面
             return RedirectResponse(
                 url="/member",
                 status_code=303
             )
     return RedirectResponse(
+        # 登入資料輸入錯誤後導向錯誤頁面，並傳入錯誤訊息
         url="/ohoh?msg=信箱或密碼輸入錯誤",
         status_code=303
     )
-    
+
+# 接入使用者頁面
 @app.get("/member")
 async def member(request: Request):
+    # 進入使用者頁面，先用middleware檢查登入狀態
     if request.session.get("LOGGED-IN", True):
         return templates.TemplateResponse(
             request=request,
             name="member.html"
         )
+    # 若未登入則導向首頁
     return RedirectResponse(
         url="/",
         status_code=303
     )
 
+# 接入錯誤頁面
 @app.get("/ohoh")
 async def ohoh(request: Request, msg: str = ""):
     return templates.TemplateResponse(
@@ -87,6 +99,7 @@ async def ohoh(request: Request, msg: str = ""):
         context={"msg": msg}
     )
 
+# 在使用者頁面選擇登出，將登入狀態設為false，避免使用者能在未登入的狀態下進入使用者頁面
 @app.get("/logout")
 async def logout(request: Request):
     request.session["LOGGED-IN"] = False
@@ -94,6 +107,7 @@ async def logout(request: Request):
         url="/"
     )
 
+# 接入旅館頁面，使用抓下來的資料進行比對，回傳對應的結果
 @app.get("/hotel/{hotelId}")
 async def hotel_search(request: Request, hotelId: int):
     if hotelId not in hotelsDict:
@@ -103,6 +117,7 @@ async def hotel_search(request: Request, hotelId: int):
             name="hotel.html",
             context=context
         )
+    # 用context傳遞資料給html頁面
     context = {
         "hotel_name_ch" : hotelsDict[hotelId]["hotelNameCH"],
         "hotel_name_en" : hotelsDict[hotelId]["hotelNameEN"],
