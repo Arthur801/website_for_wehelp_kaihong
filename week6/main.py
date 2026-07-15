a7ed9e9cd64c511d435d52ca588b2520bd3ddc32
Arthur801 <59018955+Arthur801@users.noreply.github.com>
2026-07-15 09:35:34 +0800

    Finish session in login and member in week6
    Changed: week6 main.py

--- week6/main.py
+++ week6/main.py
@@ -18,10 +18,6 @@ app.add_middleware(SessionMiddleware, secret_key="gjapsfu1")
 # 取得jinja2模板資料夾（html)
 templates = Jinja2Templates(directory="templates") # 同第16行
 
-# 模擬使用者帳號資料庫
-# USER_DATABASE = {
-#     "abc@abc.com" : "abc"
-# }
 # 建立db與tables
 websiteDB = mysql.connector.connect(
     host="localhost",
@@ -69,13 +65,11 @@ async def signup(request: Request, userName: Annotated[str, Form()],
             url="/ohoh?msg=重複的電子郵件",
             status_code=303
         )
-    # 若資料庫中member不存在相同的email，成功註冊，將資料輸入資料庫，導回首頁並將session變成已登入狀態，使用%s傳入引數
+    # 若資料庫中member不存在相同的email，成功註冊，將資料輸入資料庫並導回首頁，使用%s傳入引數
     else:
         signupCursor.execute("INSERT INTO member (name, email, password) VALUE (%s, %s, %s)", (userName, email, password))
         websiteDB.commit()
         signupCursor.close()
-        # 這邊可能需要改成存是哪一個user登入的資訊
-        request.session["LOGGED-IN"] = True
         return RedirectResponse(
             url="/",
             status_code=303
@@ -89,35 +83,41 @@ async def login(request: Request,
     # 建一個登入專用cursor，檢查登入資料與資料庫中的member email 和password 是否吻合，是則導向member page，否則導向error page
     loginCursor = websiteDB.cursor()
     loginCursor.execute("SELECT * FROM member WHERE email = %s AND password = %s LIMIT 1", (email, password))
+    userData = loginCursor.fetchone()
+    if userData is not None:
+        # session存member ID 和姓名
+        userID, userName = userData[0], userData[1]
+        request.session["userID"] = userID
+        request.session["userName"] = userName
+        loginCursor.close()
 
-    # if email in USER_DATABASE:
-    #     if password == USER_DATABASE[email]:
-    #         request.session["LOGGED-IN"] = True
-    #         # 判斷登入成功後導向使用者頁面
-    #         return RedirectResponse(
-    #             url="/member",
-    #             status_code=303
-    #         )
-    # return RedirectResponse(
-    #     # 登入資料輸入錯誤後導向錯誤頁面，並傳入錯誤訊息
-    #     url="/ohoh?msg=信箱或密碼輸入錯誤",
-    #     status_code=303
-    # )
+        return RedirectResponse(
+            url=f"/member/{userName}",
+            status_code=303
+        )
+    else:
+        loginCursor.close()
+        return RedirectResponse(
+            url="/ohoh?msg=電子郵件或密碼錯誤",
+            status_code=303
+        )
 
 # 接入使用者頁面
-@app.get("/member")
-async def member(request: Request):
+@app.get("/member/{userName}")
+async def member(request: Request, userName):
     # 進入使用者頁面，先用middleware檢查登入狀態
-    if request.session.get("LOGGED-IN", True):
+    if request.session.get("userName") is not None:
         return templates.TemplateResponse(
             request=request,
-            name="member.html"
+            name="member.html",
+            context={"userName": userName}
         )
     # 若未登入則導向首頁
-    return RedirectResponse(
-        url="/",
-        status_code=303
-    )
+    else:
+        return RedirectResponse(
+            url="/",
+            status_code=303
+        )
 
 # 接入錯誤頁面
 @app.get("/ohoh")
